9de2c2797177e9a47c21ad9af92a008070ad6507
Ariasnahuel23 <ariasnahuel2323@gmail.com>
2025-01-23 20:09:54 -0300

    Primer Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <h1 className="title">Dashboard</h1>
+      </main>
+      <footer className="">
+     
+      </footer>
+    </div>
+  );
+}
